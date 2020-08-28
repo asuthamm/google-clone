e10589ddfe4dad7e0fa9f09105b1866e0be01257
asuthamm <asuthamm@gmail.com>
2020-08-27 23:14:21 -0400

    add Home.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Google</h1>
-      {/* Home - entire home page */}
-      <Home />
-      
-      {/* SearchPage - result of the search page */}
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>Search Page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
 
 function Home() {
   return (
     <div className="home">
-      <h1> home page</h1>
+      {/* <h1> home page</h1> */}
+
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to='/about'>About</Link>
+          <Link to='/store'>Store</Link>
+        </div>
+        <div className="home__headerRight">
+          <Link to='/gmail'>Gmail</Link>
+          <Link to='/images'>Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+
+      <div className="home__body">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="google logo" />
+
+        <div className="home__inputContainer">
+          
+        </div>
+      </div>
+
     </div>
   );
 }
