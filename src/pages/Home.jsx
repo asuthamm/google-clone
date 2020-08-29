04336d1f42f1051fbeeafbc7d01ea965c1e45db9
asuthamm <asuthamm@gmail.com>
2020-08-29 16:33:19 -0400

    add search comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import AppsIcon from "@material-ui/icons/Apps";
 import { Avatar } from "@material-ui/core";
+import Search from './Search';
 
 function Home() {
   return (
@@ -23,10 +24,13 @@ function Home() {
       </div>
 
       <div className="home__body">
-        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="google logo" />
+        <img 
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="google logo"  
+        />
+
 
         <div className="home__inputContainer">
-          
+          <Search />
         </div>
       </div>
 
---
+++ src/pages/Search.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import './Search.css'
+import SearchIcon from "@material-ui/icons/Search"; 
+import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+function Search({ hideButton=false }) {
+  const [input, setInput] = useState('')
+  const history = useHistory();
+
+  const search = e => {
+    e.preventDefault();
+    console.log(input)
+    history.push('/search')
+  }
+
+  return (
+    <div className='search'>
+      <div className="search__input">
+        <SearchIcon className='search__inputIcon' />
+        <input value={input} onChange={ e => 
+          setInput(e.target.value)} />
+        <MicIcon />
+      </div>      
+
+  
+      {/* <div className="search__buttons">
+        <Button onClick={search} variant="outlined">
+          Google Search
+        </Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
+      </div> */}
+    </div>
+  )
+}
+
+export default Search
