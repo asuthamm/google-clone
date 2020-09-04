513fe2eaacc50aba4b7b8f616485c43a09ca2e2d
asuthamm <asuthamm@gmail.com>
2020-09-03 20:17:08 -0400

    add SearchPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import SearchPage from './pages/Search'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <Router>
         <Switch>
           <Route path="/search">
-            <h1>Search Page</h1>
+            <SearchPage />
+            <h1>x</h1>
           </Route>
           <Route path="/">
             <Home />
